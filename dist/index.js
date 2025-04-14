7ace14ffb8b0dafadeb8e3410ed0b4cdb2658fbc
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2025-04-15 00:06:50 +0600

    Basic Datatype Explore

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,18 @@
 "use strict";
-const tasting = (name) => {
-    return `Hello, ${name}!`;
+const tasting = (nam) => {
+    return `Hello, ${nam}!`;
 };
 console.log(tasting(" I ❣️ TypeScript "));
+//Data type Explore
+//string
+const fullName = "Mehedi Hasan";
+//number 
+const roll = 5;
+//boolean 
+const isActive = true;
+//null 
+const nullValue = null;
+//array 
+const fruits = ["apple", "banana", "orang"];
+//tuple
+const nameAge = ["Mithu", 25];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 
-const tasting = (name: string): string => {
-    return `Hello, ${name}!`;
+const tasting = (nam: string): string => {
+    return `Hello, ${nam}!`;
   };
   
   console.log(tasting(" I ❣️ TypeScript "));
+
+
+//Data type Explore
+//string
+const fullName:string = "Mehedi Hasan"
+
+//number 
+const roll:number = 5
+
+//boolean 
+const isActive : boolean=true
+
+//null 
+const nullValue : null=null
+
+//array 
+const fruits:string[] =["apple","banana","orang"]
+
+//tuple
+const nameAge:[string,number] =["Mithu",25]
