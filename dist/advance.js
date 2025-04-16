b5116f27e552017acc0042883eb1dbe97dab313d
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2025-04-16 12:25:47 +0600

    generic explore

--- dist/advance.js
+++ dist/advance.js
@@ -10,3 +10,21 @@ const user1 = {
     isAdmin: true,
     country: "bangladesh"
 };
+//Generic Type
+const number = [1, 2, 3, 4];
+const text = ['a', 'b', 'c'];
+const boolean = [true, false];
+const colors2 = ['red', 'green', 'blue'];
+const students = [
+    {
+        name: "Mehedi Hasan",
+        age: 24,
+        roll: 2
+    },
+    {
+        name: "Nahid Hasan",
+        age: 13,
+        roll: 4
+    }
+];
+const player = [11, { name: "Neymar JR", age: 28 }];
--- src/advance.ts
+++ src/advance.ts
@@ -20,3 +20,34 @@ const user1:User4 ={
     isAdmin:true,
     country: "bangladesh"
 }
+
+//Generic Type
+const number : Array<number> =[1,2,3,4]
+const text : Array<string>=['a','b','c']
+const boolean:Array<boolean> =[true, false]
+
+//dynamic Generic type 
+type GenericArray<T> =Array<T>
+const colors2:GenericArray<string>=['red','green','blue']
+
+
+
+
+//Generic dynamic array of Object
+type StudentType ={name:string,age:number,roll:number}
+const students:GenericArray<StudentType>=[
+    {
+        name:"Mehedi Hasan",
+        age:24,
+        roll:2
+    },
+    {
+        name:"Nahid Hasan",
+        age:13,
+        roll:4
+    }
+]
+
+//Generic Tuple
+type GenericTuple<X, Y>= [X,Y]
+const player:GenericTuple<number,{name:string,age:number}> =[11,{name:"Neymar JR",age:28}]
